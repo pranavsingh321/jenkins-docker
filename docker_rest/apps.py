4821877c92ff7db6134bdfa58ba95a7b86ecc2ea
pranav <pranavsingh321@gmail.com>
2018-02-07 06:43:20 +0000

    rest commit

---
+++ docker_rest/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DockerRestConfig(AppConfig):
+    name = 'docker_rest'
--- jenkinsDock/urls.py
+++ jenkinsDock/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
+from jenkins_rest import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', TemplateView.as_view(template_name="index.html")),
+    url(r'api-auth/', include('rest_framework.urls', namespace='rest_framework')), 
 ]
